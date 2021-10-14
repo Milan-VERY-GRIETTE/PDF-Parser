0118cba4217e3c4496fb44978ab9972b63e3bf42
Milan-VERY-GRIETTE <90772567+Milan-VERY-GRIETTE@users.noreply.github.com>
2021-10-14 20:08:42 +0200

    feat: argument reading and check

--- src/pdf-parser.cpp
+++ src/pdf-parser.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::cout << "--- Parseur PDF d'articles en plain texte ---" << std::endl;
+    if (argc < 2) {
+        std::cerr << "Erreur. Chemin vers le dossier non fourni." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << argv[1] << std::endl;
+
     return 0;
 }
