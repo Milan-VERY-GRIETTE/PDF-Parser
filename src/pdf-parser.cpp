ddf064d9a7012c8727deef5c9d9b0fcca516ff77
Anthony NAVARRO <anthony.navarro@proarchives.fr>
2021-10-16 14:52:52 +0200

    Lecture d'un fichier

--- src/pdf-parser.cpp
+++ src/pdf-parser.cpp
@@ -1,5 +1,29 @@
+using namespace std;
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include<algorithm>
+
+
+void findTitle(string path) {
+    
+    string ligne;
+    ifstream monFlux(path);
+
+    if(monFlux)  
+    {
+        cout << "La lecture du fichier est prête." << endl;
+    } else {
+        cerr << "ERREUR : Impossible d'ouvrir le fichier." << endl;
+    }
+
+}
+
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    string path = "data/Boudin-Torres-2006.txt";
+    findTitle(path);
     return 0;
 }
