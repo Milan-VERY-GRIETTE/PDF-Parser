a6bc19707ef42aead1394d4b8722092d4ba7e70d
AzuraGit <theo.quezel-perron@alumni.univ-avignon.fr>
2021-10-19 18:39:39 +0200

    Ajout de documentation et optimisation du code
    Optimisation des fonctions de recherches et ajout de la gestion des échecs de recherche des mots clés

--- src/pdf-parser.cpp
+++ src/pdf-parser.cpp
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+//Fonction de déplacement dans un fichier. Prend une référence fstream et un integer représentant la ligne choisi
 std::fstream& GotoLine(std::fstream& file, unsigned int num){
     file.seekg(std::ios::beg);
     for(int i=0; i < num - 1; ++i){
@@ -16,18 +17,22 @@ std::fstream& GotoLine(std::fstream& file, unsigned int num){
     return file;
 }
 
-
+//fonction de recherche du mot clé abstract retourne un entier correspondant à la ligne ou il a été retrouvé
+//Penser au passage des fstream par référence car copie impossible
 int findAbstract(fstream &of){
     string abstract;
     string lower_abstract;
-    string s2 = "aim";
+    string s2 = "abstract";
     bool found = false;
     int line = 0;
     if (of.is_open())
     {
+        of.clear();
+        of.seekg(0);
         getline(of, abstract);
         locale loc;
         while (!found){
+            //Fonction de mise en minuscule d'une string
             for (string::size_type i=0; i<abstract.length(); ++i){
                 lower_abstract += tolower(abstract[i],loc);
             }
@@ -40,29 +45,21 @@ int findAbstract(fstream &of){
                 line++;
             }
         }
+        of.clear();
+        of.seekg(0);
+        return line;
     }
     else {
         cerr << "Erreur fichier non ouvrable" << endl;
         return 0;
     }
-    if (line != 0)
-    {
-        of.clear();
-        of.seekg(0);
-        return line;
-    }
-    else
-    {
-        of.clear();
-        of.seekg(0);
-        return 0;
-    }   
 }
 
+//fonction de recherche du mot clé introduction retourne un entier correspondant à la ligne ou il a été retrouvé
 int findIntro(fstream &of){
     string abstract;
     string lower_abstract;
-    string s2 = "1958";
+    string s2 = "introduction";
     bool found = false;
     int line = 0;
     if (of.is_open())
@@ -82,25 +79,17 @@ int findIntro(fstream &of){
                 line++;
             }
         }
+        of.clear();
+        of.seekg(0);
+        return line;
     }
     else {
         cerr << "Erreur fichier non ouvrable" << endl;
         return 0;
     }
-    if (line != 0)
-    {
-        of.clear();
-        of.seekg(0);
-        return line;
-    }
-    else
-    {
-        of.clear();
-        of.seekg(0);
-        return 0;
-    }   
 }
 
+//fonction de recherche du mot clé université/ecole retourne un entier correspondant à la ligne ou il a été retrouvé
 int findUni(fstream &of){
     string abstract;
     string lower_abstract;
@@ -116,7 +105,7 @@ int findUni(fstream &of){
             for (string::size_type i=0; i<abstract.length(); ++i){
                 lower_abstract += tolower(abstract[i],loc);
             }
-            if (lower_abstract.find(s2) != std::string::npos || lower_abstract.find(s3) != std::string::npos) {
+            if (lower_abstract.find(s2) != std::string::npos || lower_abstract.find(s3) != std::string::npos || line > 100) {
               found = true;
             }
             else{
@@ -124,18 +113,19 @@ int findUni(fstream &of){
                 getline(of, abstract);
                 line++;
             }   
-        }      
-        if (line != 0)
+        }
+              
+        if (line > 100)
         {
             of.clear();
             of.seekg(0);
-            return line;
+            return 0;
         }
         else
         {
             of.clear();
             of.seekg(0);
-            return 0;
+            return line;
         }
     }
     else {
@@ -146,6 +136,7 @@ int findUni(fstream &of){
     
 }
 
+//fonction d'extraction de contenue entre deux ligne d'un fichier passé en paramètre
 int extractAbstract(fstream &of, int start, int end){
     string abstract, extracted;
     string lower_abstract;
@@ -155,6 +146,7 @@ int extractAbstract(fstream &of, int start, int end){
     {
         while (line <= end){
             getline(of, extracted);
+            //Concatenation
             abstract = abstract + "\n" + extracted;
             line++;
         }
@@ -163,15 +155,18 @@ int extractAbstract(fstream &of, int start, int end){
     return 0;   
 }
 
-
+//Choix de la création de diverses fonction de recherche car fonctionnement différent en cas d'echec de match
 int main(int argc, char const *argv[])
 {
     fstream of;
+    //sera remplacer lors du merge
     of.open("Das_Martin.txt");
     int start = findAbstract(of);
+    //prise en compte de la possibilité d'echec de match de abstract
     if (start == 0){
         start = findUni(of);
     }
+    //la fonction intégre une gestion en cas d'echec de match du mot clé
     int end = findIntro(of);
     GotoLine(of, start);
     cout << start << " " << end << endl;
